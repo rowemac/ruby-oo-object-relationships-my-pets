c9e0125a36f75d802374a4dce721a8eebcefd8e3
Mac Rowe <m.rowe42@gmail.com>
2021-03-09 13:23:23 -0500

    commit tree e37a657a386062835c0c319f61ee10dde2882edf

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,22 @@
+require 'pry'
+
 class Cat
-  # code goes here
+
+  attr_accessor :owner, :mood
+  attr_reader :name
+
+  @@all = []
+
+  def initialize(name, owner)
+    @name = name
+    @owner = owner
+    @mood = "nervous"
+    @@all << self
+    #binding.pry
+  end
+
+  def self.all
+    @@all
+  end
+
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,84 @@
+require 'pry'
+
 class Owner
-  # code goes here
+
+  @@all = []
+
+  attr_reader :name, :species
+
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @@all << self 
+  end
+
+  def say_species
+    return "I am a #{species}."
+  end
+  
+  def self.all
+    @@all
+  end
+
+  def self.count
+    self.all.count
+  end
+
+  def self.reset_all
+    self.all.clear
+  end 
+
+  def cats
+    Cat.all.select { | cat | cat.owner == self }
+  end 
+
+  def dogs
+    Dog.all.select { | dog | dog.owner == self }
+  end
+
+  def buy_cat(name)
+    Cat.new(name, self)
+  end
+
+  def buy_dog(name)
+    Dog.new(name, self)
+  end 
+
+  def walk_dogs
+    Dog.all.select do | dog |
+      if dog.mood == "nervous"
+        dog.mood = "happy"
+      end
+    end  
+  end
+
+  def feed_cats
+    Cat.all.select do | cat |
+      if cat.mood == "nervous"
+        cat.mood = "happy"
+      end 
+    end 
+  end 
+  
+  def sell_pets
+    Cat.all.select do | cat |
+      if cat.mood == "happy"
+        cat.mood = "nervous"
+        cat.owner = nil
+      end
+    end
+
+    Dog.all.select do | dog |
+      if dog.mood == "happy"
+        dog.mood = "nervous"
+        dog.owner = nil
+      end 
+    end
+  end
+  
+  def list_pets
+    return "I have #{dogs.length} dog(s), and #{cats.length} cat(s)."
+  end
+
+
 end
